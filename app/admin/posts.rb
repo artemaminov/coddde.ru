cfc7a0b43491616bde7dd659d1d262273386657e
Artem Aminov <artemaminov@gmail.com>
2019-12-13 08:02:19 +0500

    init activeadmin

---
+++ app/admin/posts.rb
@@ -0,0 +1,32 @@
+ActiveAdmin.register Post do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :author, :title, :announce, :body, :status, tags: [:name]
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = []
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+  form do |f|
+    f.semantic_errors *f.object.errors.keys
+    f.inputs do
+      f.input :status
+      f.input :title
+      f.input :announce
+      f.input :body
+      f.has_many :tags do
+        
+      end
+    end
+    f.actions
+  end
+
+end
